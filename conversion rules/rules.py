d3e7011813e080a98399321a5ead6a4699b8d98e
Eshan Agarwal <eshanagarwal12@gmail.com>
2019-10-29 15:29:22 +0530

    Update rules.py

--- conversion rules/rules.py
+++ conversion rules/rules.py
@@ -53,5 +53,26 @@ def pos_tag():
     return pos_tags
 
 
+# This is decontracted phrase function
+
+import re
+
+def decontracted(phrase):
+    # specific
+    phrase = re.sub(r"won't", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
+
+
  
 
